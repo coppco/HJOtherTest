9aac7e8329c43d2faef68fbdf63b2e671880db8e
Coppco <coppco@qq.com>
2016-06-22 07:52:33 +0800

    删掉源码,   新增练习

---
+++ Swift练习/从零开始学习Swift-关东生/第一部分  Swift语法篇/第二章  第一个Swift程序/2.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var hj = "I'm XHJ"
+print(str)
+print(str, hj, separator: "---", terminator:"这里结束啦")
+print(str)
