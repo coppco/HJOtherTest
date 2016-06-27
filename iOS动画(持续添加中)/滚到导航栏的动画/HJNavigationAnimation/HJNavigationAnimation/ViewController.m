fdd49a22edffbaf8924996503698f0c5601ca667
xianghongjie <coppco@qq.com>
2016-06-27 18:16:24 +0800

    提交

--- iOS动画(持续添加中)/滚到导航栏的动画/HJNavigationAnimation/HJNavigationAnimation/ViewController.m
+++ iOS动画(持续添加中)/滚到导航栏的动画/HJNavigationAnimation/HJNavigationAnimation/ViewController.m
@@ -7,26 +7,68 @@
 //
 
 #import "ViewController.h"
+#import <Masonry.h>
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+/**tableView*/
+@property (nonatomic, strong)UITableView *tableView;
+/**图像*/
+@property (nonatomic, strong)UIImageView *imageV;
 @end
 
 @implementation ViewController
-
+- (UIImageView *)imageV {
+    if (!_imageV) {
+        _imageV = [[UIImageView alloc] initWithFrame:CGRectMake(0, -250, self.view.frame.size.width, 250)];
+        _imageV.contentMode = UIViewContentModeScaleAspectFit;
+        _imageV.image = [UIImage imageNamed:@"城市"];
+//        _imageV.backgroundColor = [UIColor redColor];
+    }
+    return _imageV;
+}
+- (UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = ({
+            UITableView *object = [[UITableView alloc] initWithFrame:CGRectZero style:(UITableViewStyleGrouped)];
+            object.delegate = self;
+            object.dataSource = self;
+            object.contentInset = UIEdgeInsetsMake(250, 0, 0, 0);
+            object;
+        });
+    }
+    return _tableView;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    self.edgesForExtendedLayout = UIRectEdgeNone;
+    [self configSubview];
+}
+- (void)configSubview {
+    [self.view addSubview:self.tableView];
+    [self.tableView addSubview:self.imageV];
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.bottom.equalTo(self.view);
+        make.top.mas_equalTo(250);
+    }];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-//- (void)viewDidAppear:(BOOL)animated {
-//    [super viewDidAppear:animated];
-//    [self dfddf:self.text1];
-//}
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [self.view endEditing:YES];
-    
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
 }
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:(UITableViewCellStyleValue1) reuseIdentifier:@"cell"];
+    }
+    cell.textLabel.text = indexPath.description;
+    return cell;
+}
+
+
 @end
