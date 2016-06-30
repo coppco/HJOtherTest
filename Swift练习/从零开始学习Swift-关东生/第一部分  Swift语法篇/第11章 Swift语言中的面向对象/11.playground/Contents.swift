09eec168a210914c10128aaa359ce7ac4f7feedd
Coppco <coppco@qq.com>
2016-06-30 08:05:28 +0800

    6-30

--- Swift练习/从零开始学习Swift-关东生/第一部分  Swift语法篇/第11章 Swift语言中的面向对象/11.playground/Contents.swift
+++ Swift练习/从零开始学习Swift-关东生/第一部分  Swift语法篇/第11章 Swift语言中的面向对象/11.playground/Contents.swift
@@ -64,6 +64,14 @@ struct Department {
     var no:Int = 0                     //部门编号
     var name:String = ""         //部门名称
 }
+//重载运算符
+func ==(a:Department, b:Department)->Bool {
+    return a.name == b.name && a.no == b.no
+}
+func !=(a:Department, b:Department)->Bool {
+    return a.name != b.name || a.no != b.no
+}
+
 
 //类
 class Employee {
@@ -74,6 +82,78 @@ class Employee {
     
     var department: Department?
 }
-
+//重载运算符
+func ==(a:Employee, b:Employee)->Bool {
+    return a.name == b.name && a.no == b.no && a.job! == b.job! && a.salary == b.salary && a.department! == b.department!
+}
+func !=(a:Employee, b:Employee)->Bool {
+    return a.name != b.name || a.no != b.no || a.job! != b.job! || a.salary != b.salary || a.department! != b.department!
+}
 let emp = Employee()
-let dept = Department()
+emp.job = "iOS"
+emp.name = "小王"
+emp.no = 12345
+emp.salary = 12000
+
+var dept = Department()
+dept.no = 100990
+dept.name = "研发部"
+
+//值类型和引用类型
+
+//两个闭包 分别更改类和结构体
+let changeEmployee:(Employee)->Void = { object in
+    object.name = "小李"
+}
+//在函数和闭包中修改值类型,需要使用inout关键字
+let changeDepartment:(inout Department)->Void = { (inout object:Department) in
+    object.name = "测试部"
+}
+
+print("更新前:   \(emp.name)")
+changeEmployee(emp)
+print("更新后:   \(emp.name)")
+
+print("更新前:   \(dept.name)")
+changeDepartment(&dept)
+print("更新后:   \(dept.name)")
+
+
+//===  !==   == !=
+let emp1:Employee = Employee()
+let emp2:Employee = Employee()
+emp1.job = "1"
+emp1.name = "1"
+emp1.no = 1
+emp1.salary = 1
+emp2.job = "1"
+emp2.name = "1"
+emp2.no = 1
+emp2.salary = 1
+
+if emp1 === emp2 {
+    print("引用对象相等")
+} else {
+    print("引用对象不相等")
+}
+//自定义类Employee 不能使用==和!=, 需要重载运算符, 通过定义函数的方式实现
+
+if emp1 == emp2 {
+    print("值相同")
+} else {
+    print("值不相同")
+}
+
+var dept1:Department = Department()
+var dept2:Department = Department()
+dept1.no = 1
+dept1.name = "1"
+dept2.no = 1
+dept2.name = "1"
+
+//结构体也不能使用==和!=
+if dept1 == dept2 {
+    print("值类型相同")
+} else {
+    print("值类型不相同")
+}
